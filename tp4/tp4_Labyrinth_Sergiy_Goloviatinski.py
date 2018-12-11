dc7e69b56d8d80a62c0660dd034f3e0f4f8e014b
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2018-12-11 21:52:15 +0100

    wip tp4

--- tp4/tp4_Labyrinth_Sergiy_Goloviatinski.py
+++ tp4/tp4_Labyrinth_Sergiy_Goloviatinski.py
@@ -10,8 +10,21 @@ from deap import creator
 from deap import tools
 from deap import algorithms
 
-WIDTH=5 
-HEIGHT=5
+import time
+
+WIDTH = 5
+HEIGHT = 5
+MAX_TIME = 1
+
+
+class Timer:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
 
 
 def generate_labyrinth(width, height, wall_ratio=0.3):
@@ -69,74 +82,164 @@ def display_labyrinth(grid, start_cell, end_cell, solution=None):
     plt.matshow(grid, cmap="tab20c")
     plt.show()
 
-def decodePath(start,relativePath):
-    absoluteCoorPath=[start]
+
+def decodePath(start, relativePath):
+    absoluteCoorPath = [start]
     for direction in relativePath:
         if direction == "L":
-            absoluteCoorPath.append((absoluteCoorPath[-1][0],absoluteCoorPath[-1][1]-1))
-        elif direction=="R":
-            absoluteCoorPath.append((absoluteCoorPath[-1][0],absoluteCoorPath[-1][1]+1))
-        elif direction=="D":
-            absoluteCoorPath.append((absoluteCoorPath[-1][0]+1,absoluteCoorPath[-1][1]))
-        elif direction=="U":
-            absoluteCoorPath.append((absoluteCoorPath[-1][0]-1,absoluteCoorPath[-1][1]))
+            absoluteCoorPath.append(
+                (absoluteCoorPath[-1][0], absoluteCoorPath[-1][1]-1))
+        elif direction == "R":
+            absoluteCoorPath.append(
+                (absoluteCoorPath[-1][0], absoluteCoorPath[-1][1]+1))
+        elif direction == "D":
+            absoluteCoorPath.append(
+                (absoluteCoorPath[-1][0]+1, absoluteCoorPath[-1][1]))
+        elif direction == "U":
+            absoluteCoorPath.append(
+                (absoluteCoorPath[-1][0]-1, absoluteCoorPath[-1][1]))
     return absoluteCoorPath[1:]
 
-def fitness(individual,grid,start_cell,end_cell):
-    absoluteCoorPath=decodePath(start_cell,individual)
-    fitness=0
-    for gene in absoluteCoorPath:
-        fitness+=abs(gene[0]-end_cell[0])+abs(gene[1]-end_cell[1])
 
-    
-    fitness/=len(individual)
-    fitness+=len(individual)
-    
+def fitness(individual, grid, start_cell, end_cell):
+    absoluteCoorPath = decodePath(start_cell, individual)
+    fitness = 0
+
+    # terminal condition => return 0
+    if absoluteCoorPath[-1] == end_cell:
+        return (fitness,)
+
+    for gene in absoluteCoorPath:
+        fitness += abs(gene[0]-end_cell[0])+abs(gene[1]-end_cell[1])
+        # if gene[0] < 0 or gene[1] < 0 or gene[0] >= HEIGHT or gene[1] >= WIDTH or grid[gene] == 1:
+        #     fitness += 100000
+
+    fitness /= len(individual)
+    fitness += len(individual)
+
     return (fitness,)
 
-def generate_gene(grid,previous_cell):
-    notValid=True
-    # TO-DO: do something coherent if there are walls all around previous_cell
+
+'''
+individual: is None if this is the first gene, else it contains all the genes for an individual generated before
+'''
+def generate_gene(grid, previous_cell, individual=None):
+    notValid = True
+
+    previous_cellY = previous_cell[0]
+    previous_cellX = previous_cell[1]
+
+    choiceCount = 0
     while notValid:
-        gene=random.choice(['L','D','R','U'])
-        coord=decodePath(previous_cell,[gene])[0]
-        if coord[0] >= 0 and coord[1] >= 0 and coord[0] <HEIGHT and coord[1]<WIDTH and grid[coord]!=1:
-            notValid=False
+        choiceCount += 1
+        if choiceCount == 50:
+            
+            if individual is None:
+                # display labyrinth for debug purposes to proove that indeed there is no path possible
+                display_labyrinth(grid, previous_cell, previous_cell)
+                # if the starting cell of the labyrinth is surrounded by walls
+                raise Exception('No path possible')
+            else:
+                return 'IGNORE'  # it means that the generated gene has to be ignored and is not valid
+
+        gene = random.choice(['L', 'D', 'R', 'U'])
+        coord = decodePath(previous_cell, [gene])[0]
+
+        if (coord[0] >= 0 and coord[1] >= 0 and coord[0] < HEIGHT and coord[1] < WIDTH and grid[coord] != 1
+                and not (coord[0] == previous_cellY and coord[1] == previous_cellX)):
+            notValid = False
+
+            # To prevent taking twice same cell and loop in the labyrinth
+            if individual is not None and coord in individual:
+                notValid = True
 
     return gene
 
-if __name__=="__main__":
+'''
+if finds a invalide gene (wall/outside of the grid), cuts off the genes from the invalid gene to the end of the individual dna
+TO-DO: use this after mutation
+'''
+def purge_genome(individual):
+    pass
+
+
+if __name__ == "__main__":
 
     grid, start_cell, end_cell = generate_labyrinth(WIDTH, HEIGHT)
     # donc pos min = (0,0) et pos max = (9,9)
-
     toolbox = base.Toolbox()
     toolbox.register("fitness", fitness)
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,)) #the algorithm has to minimize the fitness
-    creator.create("Individual", list, fitness=creator.FitnessMin) 
+    # the algorithm has to minimize the fitness
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMin)
     toolbox.register("mate", tools.cxUniform)
-    toolbox.register("mutate", tools.mutShuffleIndexes,indpb=0.1)
+    toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.1)
     toolbox.register("select", tools.selTournament)
-    toolbox.register("init_gene", generate_gene, grid,start_cell)
-    toolbox.register("init_individual", tools.initRepeat, creator.Individual, toolbox.init_gene, 1)
-    toolbox.register("init_population", tools.initRepeat, list, toolbox.init_individual)
-    
+    toolbox.register("init_gene", generate_gene, grid, start_cell)
+    toolbox.register("init_individual", tools.initRepeat,
+                     creator.Individual, toolbox.init_gene, 1)
+    toolbox.register("init_population", tools.initRepeat,
+                     list, toolbox.init_individual)
+
     pop = toolbox.init_population(n=10)
-   
+
     # Evaluate the entire population
-    fitnesses = map(lambda ind: toolbox.fitness(ind,grid,start_cell,end_cell),pop)
+    fitnesses = map(lambda ind: toolbox.fitness(
+        ind, grid, start_cell, end_cell), pop)
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
-        print(f"fitness:{ind.fitness.values}  {ind}")
+        # print(f"fitness:{ind.fitness.values}  {ind}")
 
+    remaining_time = MAX_TIME
+    cxProba = 0.7
+    mutProba = 0.2
+    select_nb = int(len(pop)/2)
+    tourn_size = int(len(pop)/5)
 
+    while remaining_time > 0:
+        with Timer() as t:
 
-   
+            for ind in pop:
+                ind.append(generate_gene(
+                    grid, decodePath(start_cell, ind)[-1], ind))
+                if ind[-1]=='IGNORE':
+                    ind=ind[:-1]
 
-    solution=[start_cell]
-    solution+=decodePath(start_cell,pop[0])
-    solution.append(end_cell)
+            offspring = toolbox.select(pop, select_nb, tourn_size)
 
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < cxProba:
+                    toolbox.mate(child1, child2, 0.5)
 
-    
-    display_labyrinth(grid, start_cell, end_cell)
+            for mutant in offspring:
+                if random.random() < mutProba:
+                    toolbox.mutate(mutant)
+
+            # childs = algorithms.varAnd(offspring, toolbox, cxProba, mutProba)
+
+            fitnesses = map(lambda ind: toolbox.fitness(
+                ind, grid, start_cell, end_cell), offspring)
+            for ind, fit in zip(offspring, fitnesses):
+                ind.fitness.values = fit
+
+            fitnesses = [ind.fitness.values[0] for ind in offspring]
+            min_fit = min(fitnesses)
+
+            best = pop[fitnesses.index(min_fit)]
+            pop = sorted(pop, key=lambda ind: -1*ind.fitness.values[0])
+            pop[:select_nb] = offspring
+
+            winners = [ind for ind in pop if ind.fitness.values[0] == 0.0]
+
+            if len(winners) > 0:
+                break
+
+        remaining_time -= t.interval
+
+    winners.append(best)
+    for ind in winners:
+        # solution = [start_cell]
+        solution = decodePath(start_cell, ind)
+        
+        display_labyrinth(grid, start_cell, end_cell, solution)
